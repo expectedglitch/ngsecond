0555cfa37fa78c3593843ab8eed3aa109bc85dc0
expectedglitch <listva.aw@gmail.com>
2021-07-19 00:28:00 +0200

    changes server.js script

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/t1'));
 
 app.all('*', (req, res) => {
-    res.status(200).sendFile(__dirname + '/dist/index.html');
+    res.status(200).sendFile(__dirname + '/dist/t1/index.html');
 });
 
 app.listen(process.env.PORT || 8080);
