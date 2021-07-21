b25eccc0215a20c44a2f1878d1659cc5cfb26aca
expectedglitch <listva.aw@gmail.com>
2021-07-21 12:42:21 +0200

    third attempt 21_07

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC9Xo3uNLQ23tk02whgjYk5nemnUAa6Qb8",
+    authDomain: "fir-demo-9d4a8.firebaseapp.com",
+    projectId: "fir-demo-9d4a8",
+    storageBucket: "fir-demo-9d4a8.appspot.com",
+    messagingSenderId: "207843119854",
+    appId: "1:207843119854:web:675a05457cd8fb357b59ff",
+    measurementId: "G-T6FLWD3Y9P"
+  }
 };
